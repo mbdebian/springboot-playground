567b055a2de1338814360c47f7d7dc7d3dc72c38
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-17 10:52:20 +0000

    resource dispatch test

--- building_microservices_with_springboot/spring-microservices-application/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesApplication.java
+++ building_microservices_with_springboot/spring-microservices-application/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesApplication.java
@@ -1,17 +1,23 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class SpringMicroservicesApplication {
+
+	@Autowired
+	@Resource(name = "cats")
+	public List<String> list;
 
 	@Bean
 	public List<String> cats() {
@@ -20,7 +26,7 @@ public class SpringMicroservicesApplication {
 
 	@RequestMapping("/")
 	public String message() {
-		return "Hello world";
+		return String.join(", ", list);
 	}
 
 	public static void main(String[] args) {
