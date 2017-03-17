3cd8ebb2cce573d7042b7748b256e8890c69a785
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-17 12:17:30 +0000

    override default dispatcher servlet example

--- building_microservices_with_springboot/spring-microservices-application/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesApplication.java
+++ building_microservices_with_springboot/spring-microservices-application/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesApplication.java
@@ -3,13 +3,17 @@ package com.google.mbdebian.springboot.playground.microservices.tutorial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletRegistration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +21,19 @@ import java.util.List;
 @RestController
 @ImportResource("/simple-context.xml")
 public class SpringMicroservicesApplication {
+
+	// Override default dispatcher servlet
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean registration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/servlet/*");
+		registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME);
+		return registration;
+	}
 
 	@Autowired
 	@Resource(name = "circus")
