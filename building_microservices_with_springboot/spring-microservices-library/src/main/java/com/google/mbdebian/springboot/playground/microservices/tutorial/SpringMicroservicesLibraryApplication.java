b5ce6b5a461bedd3b6298e38b2da0a145eed6db5
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-21 11:03:35 +0000

    library microservice created

---
+++ building_microservices_with_springboot/spring-microservices-library/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesLibraryApplication.class, args);
+	}
+}
