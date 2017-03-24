a160dc249303dfb91043e1828f3a50993b51aff4
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-24 00:50:37 +0000

    using properties from the configuration server

--- building_microservices_with_springboot/spring-microservices-config-client/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesConfigClientApplication.java
+++ building_microservices_with_springboot/spring-microservices-config-client/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesConfigClientApplication.java
@@ -1,12 +1,25 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMicroservicesConfigClientApplication {
+
+	// This is going to inject the value from the configuration
+	@Value("${message}")
+	private String message;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesConfigClientApplication.class, args);
+	}
+
+	@RequestMapping("/message")
+	public String message() {
+		return this.message;
 	}
 }
