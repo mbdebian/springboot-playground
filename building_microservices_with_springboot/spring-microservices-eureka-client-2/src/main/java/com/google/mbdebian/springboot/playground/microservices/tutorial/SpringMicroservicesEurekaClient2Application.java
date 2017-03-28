ae9e76df285a8f26c0a7c69f0d00bdb0268aa33f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 00:06:43 +0100

    eureka client to explore the eureka server

---
+++ building_microservices_with_springboot/spring-microservices-eureka-client-2/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesEurekaClient2Application.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesEurekaClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaClient2Application.class, args);
+	}
+}
