b73500d1da27a8b2b09d1842cb48cf8830ac2ec3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-28 14:14:05 +0100

    logger added to the catalog application

--- building_microservices_with_springboot/spring-microservices-catalog/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesCatalogApplication.java
+++ building_microservices_with_springboot/spring-microservices-catalog/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesCatalogApplication.java
@@ -1,10 +1,13 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMicroservicesCatalogApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringMicroservicesCatalogApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesCatalogApplication.class, args);
