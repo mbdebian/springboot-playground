e0f9ade190c8f90cad70ddbf6bc219430b99ae6e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-28 22:23:06 +0100

    enable eureka server annotation

--- building_microservices_with_springboot/spring-microservices-eureka-server/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesEurekaServerApplication.java
+++ building_microservices_with_springboot/spring-microservices-eureka-server/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringMicroservicesEurekaServerApplication {
 
 	public static void main(String[] args) {
