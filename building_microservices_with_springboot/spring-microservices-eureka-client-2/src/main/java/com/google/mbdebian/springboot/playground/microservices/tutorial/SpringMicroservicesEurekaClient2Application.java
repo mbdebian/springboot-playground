cc95251cddf2437e2c538bfa128d3e3e6360b3e2
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 00:16:01 +0100

    eureka client and rest controller annotation and method

--- building_microservices_with_springboot/spring-microservices-eureka-client-2/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesEurekaClient2Application.java
+++ building_microservices_with_springboot/spring-microservices-eureka-client-2/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesEurekaClient2Application.java
@@ -1,10 +1,27 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class SpringMicroservicesEurekaClient2Application {
+
+	@Autowired
+	private EurekaClient client;
+
+	@RequestMapping("/serverinfo")
+	public String serviceInfo() {
+		InstanceInfo instance = client.getNextServerFromEureka("MyClient", false);
+		return instance.getHomePageUrl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesEurekaClient2Application.class, args);
