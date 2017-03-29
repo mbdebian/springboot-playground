0a8250afac11331733472c25b53c6a97f70626ba
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 14:24:37 +0100

    example timeout configuration for hystrix

--- building_microservices_with_springboot/spring-microservices-hystrix/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesHystrixApplication.java
+++ building_microservices_with_springboot/spring-microservices-hystrix/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesHystrixApplication.java
@@ -1,12 +1,14 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,13 +29,16 @@ public class SpringMicroservicesHystrixApplication {
 	private RestTemplate restTemplate;
 
 	@RequestMapping("/startClient")
-	@HystrixCommand(fallbackMethod = "failover")
-	public List<String> startClient() {
+	@HystrixCommand(fallbackMethod = "failover", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+	})
+	public List<String> startClient(@RequestParam long time) throws InterruptedException {
+		Thread.sleep(time);
 		return this.restTemplate.getForObject("http://localhost:8888/service", List.class);
 	}
 
-	public List<String> failover() {
-		return Arrays.asList("Default1", "Default2");
+	public List<String> failover(long time) {
+		return Arrays.asList("Default1", "Default2", Long.toString(time));
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesHystrixApplication.class, args);
