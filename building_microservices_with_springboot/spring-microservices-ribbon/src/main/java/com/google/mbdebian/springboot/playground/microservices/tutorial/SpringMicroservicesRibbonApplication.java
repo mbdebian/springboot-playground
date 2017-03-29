1bd00d2bab814e497872d70bf75ecf235d9abcc2
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 19:56:55 +0100

    client pointing to localhost:7777 for a test

--- building_microservices_with_springboot/spring-microservices-ribbon/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesRibbonApplication.java
+++ building_microservices_with_springboot/spring-microservices-ribbon/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesRibbonApplication.java
@@ -1,10 +1,29 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class SpringMicroservicesRibbonApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	public RestTemplate restTemplate;
+
+	@RequestMapping("/startClient")
+	public String startClient() {
+		return this.restTemplate.getForObject("http://localhost:7777/execute", String.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesRibbonApplication.class, args);
