3e42308748273aeb038c768eb8c29e737e9aadd0
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 16:29:28 +0100

    Hystrix dashboard enabled

--- building_microservices_with_springboot/spring-microservices-hystrix/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesHystrixApplication.java
+++ building_microservices_with_springboot/spring-microservices-hystrix/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesHystrixApplication.java
@@ -5,7 +5,8 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +18,10 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
-@EnableCircuitBreaker
+// Annotation removed for hystrix dashboard
+//@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class SpringMicroservicesHystrixApplication {
 
 	@Bean
