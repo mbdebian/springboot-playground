28cb0c8b7dde15879e66c3f579142ed0317bc2d5
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-29 16:52:41 +0100

    request mappings and server port configurated in running configurations

--- building_microservices_with_springboot/spring-microservices-simple-service-2/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesSimpleService2Application.java
+++ building_microservices_with_springboot/spring-microservices-simple-service-2/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesSimpleService2Application.java
@@ -1,10 +1,25 @@
 package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringMicroservicesSimpleService2Application {
+
+	@Value("${server.port}")
+	public String port;
+
+	@RequestMapping("/execute")
+	public String execute() {
+		return "Hello from the port " + this.port;
+	}
+
+	@RequestMapping("/")
+	public String status() {
+		return "Up";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroservicesSimpleService2Application.class, args);
