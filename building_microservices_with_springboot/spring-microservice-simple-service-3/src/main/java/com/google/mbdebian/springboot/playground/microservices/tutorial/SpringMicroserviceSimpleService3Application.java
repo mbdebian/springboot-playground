448c9398dda2d9430bcca33e90821b48067af067
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-30 11:27:49 +0100

    simple microservice for zuul tutorial example

---
+++ building_microservices_with_springboot/spring-microservice-simple-service-3/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroserviceSimpleService3Application.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceSimpleService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceSimpleService3Application.class, args);
+	}
+}
