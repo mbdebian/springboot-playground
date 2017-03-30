20c4bf1b15aaf03a12c6449c60dcb848238469d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-30 22:20:23 +0100

    zuul microservice

---
+++ building_microservices_with_springboot/spring-microservices-zuul/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesZuulApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesZuulApplication.class, args);
+	}
+}
