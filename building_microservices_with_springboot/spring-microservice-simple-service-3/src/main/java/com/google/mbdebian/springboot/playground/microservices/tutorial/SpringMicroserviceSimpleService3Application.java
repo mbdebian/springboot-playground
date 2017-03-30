ff73b64ec02de3f8816e6c275d0ca6b054c51f1e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-30 14:18:24 +0100

    rest controller interface and port and application name configuration

--- building_microservices_with_springboot/spring-microservice-simple-service-3/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroserviceSimpleService3Application.java
+++ building_microservices_with_springboot/spring-microservice-simple-service-3/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroserviceSimpleService3Application.java
@@ -2,9 +2,17 @@ package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMicroserviceSimpleService3Application {
+
+	@RequestMapping("/execute")
+	public String execute() {
+		return "Executed...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroserviceSimpleService3Application.class, args);
