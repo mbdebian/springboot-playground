07eca73cb20d2826b1fd06d8ac22d1ca52e32fd5
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-03-30 22:52:52 +0100

    first basic zuul configuration

--- building_microservices_with_springboot/spring-microservices-zuul/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesZuulApplication.java
+++ building_microservices_with_springboot/spring-microservices-zuul/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesZuulApplication.java
@@ -2,8 +2,10 @@ package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringMicroservicesZuulApplication {
 
 	public static void main(String[] args) {
