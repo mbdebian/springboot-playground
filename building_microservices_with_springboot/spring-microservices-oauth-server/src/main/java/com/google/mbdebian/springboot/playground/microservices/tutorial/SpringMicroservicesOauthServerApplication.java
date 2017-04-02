67a4041346d195dd78ca581e65d1fe527c458e0a
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-02 23:04:52 +0100

    enable the authorization server

--- building_microservices_with_springboot/spring-microservices-oauth-server/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesOauthServerApplication.java
+++ building_microservices_with_springboot/spring-microservices-oauth-server/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesOauthServerApplication.java
@@ -2,8 +2,10 @@ package com.google.mbdebian.springboot.playground.microservices.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class SpringMicroservicesOauthServerApplication {
 
 	public static void main(String[] args) {
