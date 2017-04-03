6d14004d6afb0bd8842c6d3ddb234eb2eb169c2e
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-03 15:26:00 +0100

    oAuth2 client based on Spring Boot

---
+++ building_microservices_with_springboot/spring-microservices-oauth-client/src/main/java/com/google/mbdebian/springboot/playground/microservices/tutorial/SpringMicroservicesOauthClientApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesOauthClientApplication.class, args);
+	}
+}
