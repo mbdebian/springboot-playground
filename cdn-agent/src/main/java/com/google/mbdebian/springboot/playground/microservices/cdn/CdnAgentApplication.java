51a2e2ad104d9e75b47c0b9eec804f9c9d954785
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-06 10:53:28 +0100

    First working version, missing file name

--- cdn-agent/src/main/java/com/google/mbdebian/springboot/playground/microservices/cdn/CdnAgentApplication.java
+++ cdn-agent/src/main/java/com/google/mbdebian/springboot/playground/microservices/cdn/CdnAgentApplication.java
@@ -1,12 +1,52 @@
 package com.google.mbdebian.springboot.playground.microservices.cdn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @SpringBootApplication
+@RestController
 public class CdnAgentApplication {
+    private static final Logger logger = LoggerFactory.getLogger(CdnAgentApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(CdnAgentApplication.class, args);
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    // Serve the file via HTTP
+    @RequestMapping("/getFile")
+    public ResponseEntity<?> getFile() throws IOException {
+        // Resource
+        File file = applicationContext.getResource("static/images-2.jpeg").getFile();
+        logger.info("Resource at: " + file.getAbsolutePath());
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        // HTTP Headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Content-Disposition", "attachment");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octect-stream"))
+                .body(resource);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdnAgentApplication.class, args);
+    }
 }
