b2650af570d8727deb35eabc7764de573284beb2
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-06 06:03:51 +0100

    microservice for distributing files

---
+++ cdn-agent/src/test/java/com/google/mbdebian/springboot/playground/microservices/cdn/CdnAgentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.google.mbdebian.springboot.playground.microservices.cdn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdnAgentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
