6a9071725ee0775f5625de5adbc1279a2d9ac457
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-07 04:48:24 +0100

    zuul testing service

---
+++ zuul-test-service/src/main/java/com/google/mbdebian/springboot/playground/microservices/cdn/ZuulTestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.cdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulTestServiceApplication.class, args);
+	}
+}
