86c6862a7b628bd2a21b694634fa3f1b73fa2783
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-04-07 04:14:03 +0100

    simple service redirector

---
+++ redirector-simple-service/src/main/java/com/google/mbdebian/springboot/playground/microservices/cdn/RedirectorSimpleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.cdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectorSimpleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectorSimpleServiceApplication.class, args);
+	}
+}
