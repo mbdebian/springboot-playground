3387f1efc690605a38695f72cf77b5e0d6210cf9
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-03 09:33:43 +0100

    add word rest method

--- hackpersistence/src/main/java/com/google/mbdebian/springboot/playground/microservices/hacking/persistence/HackpersistenceApplication.java
+++ hackpersistence/src/main/java/com/google/mbdebian/springboot/playground/microservices/hacking/persistence/HackpersistenceApplication.java
@@ -1,10 +1,24 @@
 package com.google.mbdebian.springboot.playground.microservices.hacking.persistence;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HackpersistenceApplication {
+
+	@Autowired
+	private WordMemory wordMemory;
+
+	@RequestMapping("/add")
+	public String addWord(@RequestParam(value = "word", defaultValue = "defaultWord") String word) {
+		wordMemory.addWord(word);
+		return String.format("Word '%s' added to the list of words", word);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackpersistenceApplication.class, args);
