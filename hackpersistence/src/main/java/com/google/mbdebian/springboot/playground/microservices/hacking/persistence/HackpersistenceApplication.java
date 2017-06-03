b37447d351cbde42c84a4a118c8adc3cc17bbe77
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-03 09:14:22 +0100

    source code skeleton

---
+++ hackpersistence/src/main/java/com/google/mbdebian/springboot/playground/microservices/hacking/persistence/HackpersistenceApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.springboot.playground.microservices.hacking.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackpersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackpersistenceApplication.class, args);
+	}
+}
