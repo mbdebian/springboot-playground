5609619ec94c10e332a7852a37291080a526da48
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-06-29 10:38:31 +0100

    spring boot experiments project for testing pivotal book quiz

---
+++ pivotal_book/springboot-experiments/src/main/java/com/google/mbdebian/pivotal/certification/book/experiments/SpringbootExperimentsApplication.java
@@ -0,0 +1,12 @@
+package com.google.mbdebian.pivotal.certification.book.experiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExperimentsApplication.class, args);
+	}
+}
