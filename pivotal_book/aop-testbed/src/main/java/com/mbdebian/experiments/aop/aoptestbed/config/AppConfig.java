1788ffbcd5814014d66be8eea30661f651982ef3
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-19 11:36:43 +0100

    application configuration

---
+++ pivotal_book/aop-testbed/src/main/java/com/mbdebian/experiments/aop/aoptestbed/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.mbdebian.experiments.aop.aoptestbed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author Manuel Bernal Llinares
+ * Project: aop-testbed
+ * Package: com.mbdebian.experiments.aop.aoptestbed.config
+ * Timestamp: 2017-07-19 10:24
+ * ---
+ * © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+ * All rights reserved.
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AppConfig {
+}
