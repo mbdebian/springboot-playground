5708b8327556750da978981861be6f667767ea8b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-19 11:36:09 +0100

    spring application main

---
+++ pivotal_book/aop-testbed/src/main/java/com/mbdebian/experiments/aop/aoptestbed/AopTestbedApplication.java
@@ -0,0 +1,12 @@
+package com.mbdebian.experiments.aop.aoptestbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopTestbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopTestbedApplication.class, args);
+	}
+}
